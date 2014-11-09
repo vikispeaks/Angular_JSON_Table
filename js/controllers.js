efa1c8caae0fb1fd91ea871fe35427f9c740f6e5
vikispeaks <vign.ran@gmail.com>
2014-11-09 22:01:36 +0530

    Dev Files Update

---
+++ js/controllers.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+angular.module('Testing', ['ui.bootstrap'])
+    .controller('gridController', ['$scope','dataService', function($scope,dataService) {
+        //$scope.testdata="yash";
+		$scope.gridData;
+        dataService.getData().then(function(response) {
+											console.log(response);
+            $scope.gridData = response;
+          
+        });
+		
+		
+		
+		 $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+
+   }])
---
+++ js/service.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+angular.module('Testing')
+    .factory('dataService', ['$http','$q', function($http,$q) {
+        function _getData() {
+            var deferred = $q.defer();
+           // var portfolioID = portfolioService.getPortfolioId($location.path());
+            var url = '/Angular/data/data.json';
+            $http.get(url).then(
+                function(response) {
+                    deferred.resolve(response.data.table_data);
+                },
+                function(response) {
+                    deferred.reject(response);
+                }
+            );
+            return deferred.promise;
+        }
+        return {
+            getData: _getData
+        };
+    }])
